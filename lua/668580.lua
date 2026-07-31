95c03d1d887c6373569f2090645cb3e9dd5f5d14
hhuijk-hhuijkcom <297160872+hhuijk-hhuijkcom@users.noreply.github.com>
2026-07-31 10:27:33 +0000

    🧪 测试：自动生成lua文件

--- lua/668580.lua
+++ lua/668580.lua
@@ -7,10 +7,3 @@ addappid(668581,0,"b331448756a6614f7b9b52f56b71e329928370afb8034b8474e264ace0048
 --无仓库DLC
 addappid(2214824)
 addappid(2214823)
-addappid(2214822)
-addappid(2214821)
-addappid(2303120)
-addappid(2214826)
-addappid(2214820)
-addappid(2214825)
-addappid(2214830)
