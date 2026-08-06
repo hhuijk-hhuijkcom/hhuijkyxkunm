37a38d47d82f8c7d6b22d30e05ef409eddb9d631
hhuijk-hhuijkcom <xvrongzu@163.com>
2026-08-06 19:46:16 +0800

    Add files via upload

---
+++ lua/1681430.lua
@@ -0,0 +1,14 @@
+--H-huijk
+--主游戏APPID: 1681430
+addappid(1681430)  -- 主游戏
+
+--depotsID
+addappid(1681431,0,"9df4746c06e6c3240fdbbb7fafdc7ee3d4461387c34371ffcf2a5db0ac571b27")
+addappid(2436842,0,"7429f5a3d4c7fa42b6d594040def2e00bd50c410843d7ac9e33b7f5c70081f6b")
+
+--自身有仓库且所有仓库都有密钥的DLC
+addappid(2436842)
+
+--无仓库DLC
+addappid(2436840)
+addappid(2436841)
